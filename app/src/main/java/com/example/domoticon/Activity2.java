8147a97eaa7b98876e94196aed3a76dc50d180b4
Jorge García Rivera <jgarciar122@iesagora.caceres>
2024-10-07 21:23:12 +0200

    Tu mensaje de commit

---
+++ app/src/main/java/com/example/domoticon/Activity2.java
@@ -0,0 +1,16 @@
+package com.example.domoticon;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+public class Activity2 extends AppCompatActivity {
+
+    Intent intent = getIntent();
+}
